366aea92d4e3ab496ef56d27db4598513fa74de7
pavel halavach <p.halavach@student.uw.edu.pl>
2024-04-15 16:47:21 +0200

    now db is created on the start of app

--- src/main/java/dbStuff/DBConnection.java
+++ src/main/java/dbStuff/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private final static String HOST = "jdbc:mysql://localhost:3306/todolist";
+    private final static String HOST = "jdbc:mysql://localhost:3306/";
     private final static String USERNAME = "root";
     private final static String PASSWORD = "1152";
     private Connection connection;
@@ -13,6 +13,7 @@ public class DBConnection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+
 //            if (!connection.isClosed()) {
 //                System.out.println("We are connected!");
 //            }
--- src/main/java/dbStuff/DBMethods.java
+++ src/main/java/dbStuff/DBMethods.java
@@ -13,14 +13,20 @@ public class DBMethods {
     private final static String UPDATE = "UPDATE list SET title=?, description=?, dueDate=? WHERE id=?";
     private final static String GET_ONE = "SELECT * FROM list WHERE title=?";
     private static final String DELETE = "DELETE FROM list WHERE title=?";
+    private static final String CREATE_DB = "CREATE DATABASE IF NOT EXISTS `todolist`";
+    private static final String USE_DB = "USE `todolist`";
     private static final String CREATE = "CREATE TABLE IF NOT EXISTS `list` ( `id` int NOT NULL AUTO_INCREMENT, `title` varchar(100) NOT NULL," +
             " `description` varchar(300) DEFAULT NULL, `attach` longblob, `dueDate` date NOT NULL, `createdAt` date NOT NULL,  PRIMARY KEY (`id`)," +
             "  UNIQUE KEY `id_UNIQUE` (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
     private final DBConnection worker = new DBConnection();
 //    Создаю в конструкторе to do list таблицу
     public DBMethods(){
-        try (PreparedStatement preparedStatement = worker.getConnection().prepareStatement(CREATE)){
-            preparedStatement.executeUpdate();
+        try (Statement statement = worker.getConnection().createStatement()){
+            statement.addBatch(CREATE_DB);
+            statement.addBatch(USE_DB);
+            statement.addBatch(CREATE);
+            statement.executeBatch();
+            statement.clearBatch();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
